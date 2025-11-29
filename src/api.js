2330dd2700e05132dd464400b653301e36548c0e
spencer-vs <isaacharu17@gmail.com>
2025-11-29 04:44:09 +0100

    version 0.6

--- src/api.js
+++ src/api.js
@@ -6,6 +6,7 @@
 
 import axios from "axios";
 import { jwtDecode } from 'jwt-decode';
+import { AuthProvider } from "./Components/context/AuthContext";
 
 // export const BASE_URL = 'http://127.0.0.1:8001'; 
 
